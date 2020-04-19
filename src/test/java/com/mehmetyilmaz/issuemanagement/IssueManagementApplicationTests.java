6c069a44741171579b3adf426a311a517f80b262
mehmet.yilmaz <mehmet.yilmaz2@outlook.com.tr>
2020-04-19 19:34:15 +0300

    error of swagger and done

--- src/test/java/com/mehmetyilmaz/issuemanagement/IssueManagementApplicationTests.java
+++ src/test/java/com/mehmetyilmaz/issuemanagement/IssueManagementApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mehmetyilmaz.issuemanagement;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
