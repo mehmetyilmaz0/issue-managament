56230e81fe374fbaef7739fb8cad5242843dc439
mehmet.yilmaz <mehmet.yilmaz2@outlook.com.tr>
2020-04-19 17:13:27 +0300

    definition swagger operation

--- src/main/java/com/mehmetyilmaz/issuemanagement/api/ProjectController.java
+++ src/main/java/com/mehmetyilmaz/issuemanagement/api/ProjectController.java
@@ -2,8 +2,11 @@ package com.mehmetyilmaz.issuemanagement.api;
 
 
 import com.mehmetyilmaz.issuemanagement.dto.ProjectDTO;
+import com.mehmetyilmaz.issuemanagement.entity.Project;
 import com.mehmetyilmaz.issuemanagement.service.impl.ProjectServiceImpl;
 import com.mehmetyilmaz.issuemanagement.util.ApiPaths;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +14,7 @@ import javax.validation.Valid;
 
 @RestController // bu class 'in bir rest contoller oldugunu etiketleyen bir anotation
 @RequestMapping(ApiPaths.ProjectCtrl.CTRL) // localhost:8080/project ile bu classin metodlarina ulasilmasi saglandi
+@Api(value = "Project APIs") // swagger ile dokumantasyonda gorulmesi icin kullanilan anotation. Controller'lari tanimlar.
 public class ProjectController {
 
     private final ProjectServiceImpl projectServiceImpl;
@@ -23,6 +27,7 @@ public class ProjectController {
     // her entity imizin uzerinde belirli bilgileri bulundaracagimiz bir class
 
     @GetMapping("/{id}") // bu metodun webservis metoduna getirilmesi icin kullanilan anotatin
+    @ApiOperation(value = "Get By Id Operatation", response = ProjectDTO.class) // swagger ile dokumantasyonda gorulmesi icin kullanilan anotation. metod'lari tanimlar.
     public ResponseEntity<ProjectDTO> getById (@PathVariable("id") Long id) {
         ProjectDTO projectDTO = projectServiceImpl.getById(id);
         return ResponseEntity.ok(projectDTO);
@@ -31,16 +36,19 @@ public class ProjectController {
     //RequestBody : create olacak data'lar request body uzerinden gidiyor
     //Valid : ProjectDTO uzerinde tanimladigimiz validatitonlari calistirmasina yariyor.
     @PostMapping()
+    @ApiOperation(value = "Create Operatation", response = ProjectDTO.class)
     public ResponseEntity<ProjectDTO> createProejct (@Valid @RequestBody ProjectDTO projectDTO) {
         return ResponseEntity.ok(projectServiceImpl.save(projectDTO));
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update Operatation", response = ProjectDTO.class)
     public ResponseEntity<ProjectDTO> updateProject(@PathVariable("id") Long id, @Valid @RequestBody ProjectDTO projectDTO) {
         return ResponseEntity.ok(projectServiceImpl.update(id, projectDTO));
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete Operatation", response = Boolean.class)
     public ResponseEntity<Boolean> deleteProject (@PathVariable("id") Long id) {
         return ResponseEntity.ok(projectServiceImpl.delete(id));
     }
--- src/main/java/com/mehmetyilmaz/issuemanagement/dto/ProjectDTO.java
+++ src/main/java/com/mehmetyilmaz/issuemanagement/dto/ProjectDTO.java
@@ -1,5 +1,7 @@
 package com.mehmetyilmaz.issuemanagement.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +11,17 @@ import javax.validation.constraints.NotNull;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "Project Data Transfer Object") // swagger ile dokumantasyonda gorulmesi icin kullanilan anotation. dto'lari tanimlar.
 public class ProjectDTO {
 
+    @ApiModelProperty(value = "Project ID")
     private Long id;
 
     @NotNull
+    @ApiModelProperty(value = "Name of Project", required = true) // swagger ile dokumantasyonda gorulmesi icin kullanilan anotation. field'lari tanimlar.
     private String projectName;
 
     @NotNull
+    @ApiModelProperty(value = "Code of Project", required = true)
     private String projectCode;
 }
