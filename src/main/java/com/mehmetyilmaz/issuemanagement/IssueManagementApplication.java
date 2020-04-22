4eca912651d1dd811dfaa85badf8b3c225a3bd4c
mehmet.yilmaz <mehmet.yilmaz2@outlook.com.tr>
2020-04-22 09:10:27 +0300

    test datasi olusturma

--- src/main/java/com/mehmetyilmaz/issuemanagement/IssueManagementApplication.java
+++ src/main/java/com/mehmetyilmaz/issuemanagement/IssueManagementApplication.java
@@ -6,6 +6,9 @@ import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 public class IssueManagementApplication {
@@ -20,6 +23,15 @@ public class IssueManagementApplication {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		return modelMapper;
+	}
+
+	// uygulama ayaga kalkerken test datalarini otomatik yuklesin diye projects.json ile birlikte yazildi.
+	// surekli data insert etmektense bu sekilde bir cozum bulundu. sonrasinda silebiliriz.
+	@Bean
+	public Jackson2RepositoryPopulatorFactoryBean populatorFactoryBean () {
+		Jackson2RepositoryPopulatorFactoryBean factoryBean = new Jackson2RepositoryPopulatorFactoryBean();
+		factoryBean.setResources(new Resource[]{ new ClassPathResource("projects.json")});
+		return factoryBean;
 	}
 
 }
